58c999d1b311e192e0335f5280b49203229f6bbc
Alex Matsui <oleksii.matsui@gmail.com>
2023-05-25 12:41:43 +0300

    feat: sorting

---
+++ sorting.js
@@ -0,0 +1,38 @@
+//sorting
+$( "thead" ).on( "click", "th[colspan='1']", function(){
+    let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    let name = this.getAttribute("name");
+    table = document.getElementById("accounting-table");
+    switching = true;
+    dir = "asc"; 
+    while (switching) {
+     switching = false;
+     rows = table.rows;
+     for (i = 2; i < (rows.length - 1); i++) {
+       shouldSwitch = false;
+       x = rows[i].querySelector("[name='"+name+"_']");
+       y = rows[i + 1].querySelector("[name='"+name+"_']");
+       if (dir == "asc") {
+         if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+           shouldSwitch= true;
+           break;
+         }
+       } else if (dir == "desc") {
+         if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+           shouldSwitch = true;
+           break;
+         }
+       }
+     }
+     if (shouldSwitch) {
+       rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+       switching = true;
+       switchcount ++;      
+     } else {
+       if (switchcount == 0 && dir == "asc") {
+         dir = "desc";
+         switching = true;
+       }
+     }
+   }
+ });
